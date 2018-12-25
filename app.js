f919013dd30407a179ac0061c7baaf1d73809abd
mmasser95 <mmasser95@gmail.com>
2018-12-25 22:00:59 +0000

    Rama master dejamos el server

--- app.js
+++ app.js
@@ -4,19 +4,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const api = require('./routes');
-const pug = require('pug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', api);
-app.use('/public', express.static(__dirname+'/views/js'))
-app.set('view engine', 'pug');
-app.get('/login', (req, res) => {
-  res.render('login.pug');
-});
-app.get('/index', (req, res) => {
-  res.render('index.pug')
-})
+
 
 
 module.exports= app;
